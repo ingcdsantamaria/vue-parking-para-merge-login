0522ffcd38d7c3fcd83d9e739d84e605cd5660d3
cristhian david S <87107172+ingcdsantamaria@users.noreply.github.com>
2021-10-22 22:30:21 -0500

    sadasd

---
+++ src/components/ListaResidentes.vue
@@ -0,0 +1,192 @@
+<template>
+<div>
+<section class="item">
+    
+    
+<div class="conteiner">
+  <div class="icono">
+      
+  </div>
+  
+    
+    </div>
+    
+    <MenuDropDown>
+        <template v-slot:toggler>
+          <div class="lisresi">
+            <img :src= imgUser>
+            <div class="nombre-user"><b>{{Nombre}}</b></div>
+          </div>
+        </template>
+         <MenuDropDownContent class="menu-options">
+          <MenuDropDownItem class="opcion_menu">
+            <Modal2>
+            <template v-slot:toggler>
+              <p class="opcion_menu">
+                Mas informacion
+              </p>
+              <p class="opcion_menu">
+                Editar Residente
+              </p>
+              <p class="opcion_menu">
+                Eliminar Residente
+              </p>
+            </template>
+            
+          </Modal2>
+          </MenuDropDownItem>
+           </MenuDropDownContent>   
+      </MenuDropDown>
+      
+  
+
+
+
+          
+          <!-- <MenuDropDownItem class="opcion_menu" id="Bott_cancel">
+            Cancelar
+          </MenuDropDownItem> -->
+       
+  </section>
+</div>
+
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+import user from '@/assets/user.svg'
+
+import MenuDropDownContent from '@/components/dropDown/MenuDropDownContent.vue';
+import MenuDropDown from '@/components//dropDown/MenuDropdown.vue';
+import MenuDropDownItem from '@/components//dropDown/MenuDropDownItem.vue';
+;
+import NewResidente from '@/components/NewResidente.vue';
+import Modal2 from '@/components/modal/Modal2.vue';
+import ModalContent from '@/components/modal/ModalContent.vue';
+
+export default {
+  name: 'ListaResidentes',
+  components:{
+    MenuDropDownContent,
+    MenuDropDown,
+    MenuDropDownItem,
+    NewResidente,
+    Modal2,
+    ModalContent
+
+  },
+  data(){
+
+   return{
+
+      imgUser:user
+           
+            
+        }
+    },
+    props:{
+      index: {
+      type: Number,
+    },
+    Nombre: {
+      type: String,
+      
+    },
+    
+    
+    },
+    computed: {
+    ...mapGetters('New_Residente', ['getNewResidente']),
+    residente() {
+      console.log(this.getNewResidente[this.index]);
+      return this.getNewResidente[this.index];
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '@/views/scss/_theme.scss';
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.item{
+        margin-top: 10px ;
+        margin-right: 15px;
+        border-radius: 10px;
+        padding:10px ;
+        position: relative;
+        background:$third-color;
+
+        display: flex;
+        border: 2px solid $main-color;
+        align-items: center;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+.container{
+ 
+
+        display: flex;
+         flex-direction:column;
+        justify-content: space-between;
+        // flex-direction: column;
+        align-items: center;
+        width: 50%;
+
+    
+}
+.boton{
+  margin-top: 5px;
+}
+
+.buscar{
+  display: flex;
+  margin-left: 1100px;
+}
+label{
+  color:white;
+}
+input{
+  background-color: $background-color;
+  color:$main-color ;
+  height: 10px;
+  margin-left: 5px;
+  margin-top: 5px;
+  border-radius: 5px;
+}
+
+// .usuario{
+//   margin: 20px ;
+//   width: 500px;
+//   border-radius: 10px;
+//   background-color:$secondary-color ;
+//   display:flex;
+//   flex-direction: row;
+  
+
+// }
+.nombre-user{
+  font-size: 1em;
+  color:$main-color ;
+  margin: 5px;
+}
+// .lisresi{
+ 
+// }
+// Arreglando algunos detalles
+
+</style>
---
+++ src/views/Vehicle_zone.vue
@@ -0,0 +1,247 @@
+<template>
+<div class="vehicle_zone" >
+    <Header ></Header>
+    <Navbar></Navbar>
+    <div class=" cont-flex" >
+        <div class="superior-bar">
+            <Modal2>
+                <template v-slot:toggler>
+                    <img class='Imageadd' src="@/assets/add.svg"  alt="">
+                </template>
+                <modal-content>
+                    <NewParking>
+                    </NewParking>
+                    <template v-slot:cancelar>
+                        <button class="btCancel">
+                        Cancelar
+                        </button>
+                    </template>
+                    <template v-slot:confirmar>
+                        <button @click="agregarEntrada" class="btAcept"> <!-- MIRAR LA FUNCIONALIDAD DEL BOTON -->
+                        Aceptar
+                        </button>
+                    </template>
+                </modal-content>
+            </Modal2>
+            <SearchBar class="search"></SearchBar>
+        </div>
+            <div class="vehicle_list">
+                <ZonaParqueadero  v-for=" (ItemResi,index) in resident_list" :key="index" :inf_estado="ItemResi.ocupado ?  'Lleno':'Vacio'" :index="index" :parqueadero_numero="ItemResi.residente.vehiculo[0].parqueadero.nombre_Parqueadero" tipoList="Residente" >
+
+                </ZonaParqueadero>
+            </div>
+
+            <div class="vehicle-list" >
+                <ZonaParqueadero v-for=" (ItemVisitant,index) in entradas" :key="index" :inf_estado="ItemVisitant.ocupado ?  'Lleno':'Vacio'" :parqueadero_numero="ItemVisitant.tower + ItemVisitant.apto_num + ' -V' "  >
+
+                </ZonaParqueadero>
+            </div>
+        </div>
+             <!-- <Options_zona_P>
+
+                                </Options_zona_P>   -->
+                <!-- <div class="modal"  >
+                     <info_parqueadero/>
+                </div>   -->
+                <!-- <div class="modal">
+                     <Ing_vclo_visitante/>
+                </div>
+            -->
+
+    </div>
+
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+import NewParking from '@/components/Parking/ModalNewParking.vue'
+import Header from '@/components/Header.vue'
+import Navbar from '@/components/Nav.vue'
+import SearchBar from '@/components/SearchButton.vue'
+import ZonaParqueadero from '@/components/Parking/ZonaParqueadero.vue'
+import info_parqueadero from '@/components/Modal_Info_parqueadero.vue'
+import Ing_vclo_visitante from '@/components/Mdl_Ingreso_vclo_visitante.vue'
+
+import Modal2 from '@/components/modal/Modal2.vue'
+import ModalContent from '@/components/modal/ModalContent.vue'
+
+// ---------------------------------------
+const resetData = {
+  NombreParqueadero: ''
+
+}
+export default {
+  name: 'Vehicle_zone',
+  components: {
+    Header,
+    Navbar,
+    SearchBar,
+    ZonaParqueadero,
+    // info_parqueadero,
+    // Ing_vclo_visitante,
+
+    NewParking,
+    Modal2,
+    ModalContent
+  },
+  //   provide(){
+  //     dataEntrada: () => this.dataNewParking,
+  //     updateEntrada: this.updateEntrada
+  //   },
+  data () {
+    return {
+      dataNewParking: {
+        nombreParqueadero: ''
+      }
+    }
+  },
+
+  mounted () {
+    this.$store.dispatch('entrada_salida/cargarEntradas')
+    this.$store.dispatch('inf_resident/cargar_data_resi')
+  },
+  computed: {
+
+    // ...mapState('options_zona_p',['showOptions']),
+    ...mapGetters('inf_resident', ['resident_list']),
+    ...mapGetters('entrada_salida', ['entradas'])
+  },
+
+  methods: {
+    // ...mapActions('')
+
+    // agregarEntrada () {
+    //   // console.log(this.dataNewEntrada);
+    //   this.addNewEntrada(this.dataNewEntrada)
+    //   this.resetDataEntrada()
+    //   // this.toggleModal(false);
+    // },
+    // resetDataEntrada () {
+    //   this.dataNewParking = resetData
+
+    // },
+    // updateEntrada (values) {
+    //   const { key, val } = values
+    //   this.dataNewParking[key] = val
+    // }
+    // ...mapMutations('inf_resident', ['changeShowOptions']),
+
+    // ...mapActions('inf_resident', ['cargar_data_resi']),
+    // ...mapActions('entrada_salida', ['cargar_parq_list'])
+
+  }
+}
+</script>
+
+<style lang="scss" scoped >
+    @import '@/views/scss/_theme.scss';
+
+    .main {
+
+        display:flex;
+
+    }
+
+    .cont-flex{
+
+        margin-left: 20%;
+
+        padding: 20px;
+
+    }
+
+    .prue{
+
+        color: transparent;
+    }
+
+    .vehicle-list{
+
+        padding-top: 20px;
+        /* background: orange; */
+        height: 100%;
+    }
+
+    .superior-bar{
+    display: flex;
+    justify-content: space-between;
+     position: sticky;
+    top: 0px;
+    background:$background-color;
+    z-index: 5;
+    padding-bottom: 10px;
+  }
+  .menu{
+        width: 30px;
+
+    }
+
+  .modal{
+    position: fixed;
+    display: flex; /* establish flex container */
+    justify-content: center; /* center flex items horizontally, in this case */
+    align-items: center; /* center flex items vertically, in this case */
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100%;
+    width: 100%;
+    top: 0;
+  }
+
+  .modal_2{
+    position: absolute;
+    display: flex; /* establish flex container */
+    justify-content: center; /* center flex items horizontally, in this case */
+    align-items: center; /* center flex items vertically, in this case */
+    /* background-color: rgba(0, 0, 0, 0.5); */
+    height: 100%;
+    width: 100%;
+    top: 0;
+  }
+  .opcion_M{
+
+       padding: 5px;
+       font-size: 0.9em;
+        min-width:49px;
+        cursor: pointer;
+        color: $main-color;
+        border-bottom:1px solid $main-color;
+        position: relative;
+        z-index: 100;
+
+    }
+
+    #Bott_cancel{
+        border-bottom:none;
+
+    }
+    .Imageadd{
+
+        width: 50px;
+    }
+
+  @media (max-width: 600px){
+    .superior-bar{
+      display: flex;
+
+    }
+     .Imageadd{
+        width: 40px;
+
+    }
+    .search{
+        display: flex;
+        justify-content: flex-end;
+
+    }
+    .menu{
+        width: 20px;
+
+    }
+     .opcion_M{
+        padding: 2.5px;
+        font-size: 0.7em;
+    }
+  }
+
+</style>
